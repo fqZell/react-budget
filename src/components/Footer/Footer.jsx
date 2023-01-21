001d74c648cc826504b2fa1aad14fa9ec0f11df0
Салахиев Айрат Фирнатович <251915@mck-ktits.ru>
2023-01-21 16:38:04 +0300

    Learng

---
+++ src/components/Footer/Footer.jsx
@@ -0,0 +1,23 @@
+const Footer = () => {
+    return (
+        <footer>
+            <nav>
+                <ul>
+                    <li>
+                        <a href="/">
+                            Главная
+                        </a>
+                    </li>
+
+                    <li>
+                        <a href="/about">
+                            О нас
+                        </a>
+                    </li>
+                </ul>
+            </nav>
+        </footer>
+    )
+}
+
+export default Footer;
